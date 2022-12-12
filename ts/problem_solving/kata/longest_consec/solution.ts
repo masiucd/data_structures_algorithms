edc713951247b17b84fd194dc11de5b04be408a0
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2022-12-12 19:39:19 +0100

    longest consent

--- ts/problem_solving/kata/longest_consec/solution.ts
+++ ts/problem_solving/kata/longest_consec/solution.ts
@@ -1,6 +1,12 @@
-function solution(input: string[], k: number): string {
-  return ""
+export function solution(input: string[], k: number): string {
+  let longest = ""
+  if (k > 0 && input.length >= k) {
+    for (let i = 0; i < input.length - k + 1; i++) {
+      const s = input.slice(i, i + k).join("")
+      if (s.length > longest.length) {
+        longest = s
+      }
+    }
+  }
+  return longest
 }
-
-console.log(solution(["zone", "abigail", "theta", "form", "libe", "zas"], 2))
-//  abigailtheta
