d6331faa0cf045a355e94112d98b1f64b3cb4fb1
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2022-12-13 11:37:10 +0100

    memo fib

---
+++ ts/algorithms/recursion/fib.ts
@@ -0,0 +1,19 @@
+function fib(n: number): number {
+  console.log(n)
+  if (n <= 2) return 1
+  return fib(n - 1) + fib(n - 2)
+}
+
+function fibMemo(n: number, memo: Record<number, number>): number {
+  if (n <= 2) return 1
+  if (memo[n]) {
+    return memo[n]
+  }
+  const result = fibMemo(n - 1, memo) + fibMemo(n - 2, memo)
+  memo[n] = result
+  return result
+}
+
+// console.time("start")
+// console.log(fibMemo(100, {}))
+// console.timeEnd("start")
