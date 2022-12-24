4ff192748ad30fa980823e6e3e074b686ee0f135
masiucd <ciszekmarcell@gmail.com>
2022-12-24 15:06:49 +0100

    Smallest integer RS

--- rust/src/problem_solving/kata/mod.rs
+++ rust/src/problem_solving/kata/mod.rs
@@ -1,2 +1,3 @@
 pub mod longest_consent;
 pub mod number_of_people_on_the_buss;
+pub mod smallest_int;
---
+++ rust/src/problem_solving/kata/smallest_int.rs
@@ -0,0 +1,21 @@
+pub fn solution(nums: Vec<i32>) -> i32 {
+  let mut smallest = nums[0];
+  for num in nums {
+    if num < smallest {
+      smallest = num;
+    }
+  }
+  smallest
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  #[test]
+  fn test_solution_it_works() {
+    assert_eq!(solution(vec![1]), 1);
+    assert_eq!(solution(vec![1, 2, 3]), 1);
+    assert_eq!(solution(vec![1, 2, -3]), -3);
+    assert_eq!(solution(vec![1, -2, 3]), -2);
+  }
+}
