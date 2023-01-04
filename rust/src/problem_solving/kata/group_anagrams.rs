ee227a7ba7306bdf1ea545e393c4e996303759dd
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2023-01-04 17:07:43 +0100

    groupAnagrams

---
+++ rust/src/problem_solving/kata/group_anagrams.rs
@@ -0,0 +1,12 @@
+use std::collections::HashMap;
+
+pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
+    let mut map: HashMap<String, Vec<String>> = HashMap::new();
+    for s in strs {
+        let mut chars: Vec<char> = s.chars().collect();
+        chars.sort();
+        let key = chars.into_iter().collect();
+        map.entry(key).or_insert(vec![]).push(s);
+    }
+    map.values().cloned().collect()
+}
--- rust/src/problem_solving/kata/mod.rs
+++ rust/src/problem_solving/kata/mod.rs
@@ -1,4 +1,5 @@
 pub mod clean_string;
+pub mod group_anagrams;
 pub mod longest_consent;
 pub mod number_of_people_on_the_buss;
 pub mod positive_sum;
