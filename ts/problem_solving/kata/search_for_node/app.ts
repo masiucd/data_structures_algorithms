cf023da94a22d4a4e8b03c29b4d3d0f379dbdc76
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2023-01-24 11:32:35 +0100

    search for node

---
+++ ts/problem_solving/kata/search_for_node/app.ts
@@ -0,0 +1,59 @@
+const treeData = [
+  {
+    id: 1,
+    title: "Europe",
+    parentId: null,
+    children: [
+      {
+        id: 4,
+        title: "Germany",
+        parentId: 1,
+        children: [],
+      },
+      {
+        id: 5,
+        title: "France",
+        parentId: 1,
+        children: [],
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Asia",
+    parentId: null,
+    children: [
+      {
+        id: 6,
+        title: "China",
+        parentId: 2,
+        children: [],
+      },
+      {
+        id: 7,
+        title: "Japan",
+        parentId: 2,
+        children: [],
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Africa",
+    parentId: null,
+    children: [
+      {
+        id: 8,
+        title: "Egypt",
+        parentId: 3,
+        children: [],
+      },
+      {
+        id: 9,
+        title: "Kenya",
+        parentId: 3,
+        children: [],
+      },
+    ],
+  },
+]
