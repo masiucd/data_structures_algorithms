495b05711d81bceed0bed926ad60632c41c332fe
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2023-02-07 08:23:53 +0100

    feat: Maxdepth

---
+++ go/problem_solving/leet_code/max_depth/solution.go
@@ -0,0 +1,21 @@
+package maxdepth
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return max(maxDepth(root.Left), maxDepth(root.Right)) + 1
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
