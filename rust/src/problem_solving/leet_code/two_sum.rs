b695acaea16d24d97b0143c653ea8f4a8f27143f
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2023-02-20 17:42:16 +0100

    feat: Two sum Rust + tests

--- rust/src/problem_solving/leet_code/two_sum.rs
+++ rust/src/problem_solving/leet_code/two_sum.rs
@@ -9,3 +9,19 @@ pub fn two_sum(nums: Vec<i32>, target: i32) -> (usize, usize) {
     }
     return (0, 0);
 }
+
+#[cfg(test)]
+
+mod tests {
+
+    use super::*;
+
+    #[test]
+
+    fn test_two_sum() {
+        assert_eq!(two_sum(vec![2, 3, 1, 4], 6), (0, 3));
+        assert_eq!(two_sum(vec![2, 3, 1, 4], 3), (0, 2));
+        assert_eq!(two_sum(vec![2, 3, 1, 5], 7), (0, 3));
+        assert_eq!(two_sum(vec![2, 3, 1, 5], 4), (1, 2));
+    }
+}
