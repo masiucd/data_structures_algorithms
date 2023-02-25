ba03b437e8048b5a88e9361825a1173c1900f288
masiucd <ciszekmarcell@gmail.com>
2023-02-25 10:41:16 +0100

    feat: Roman to integer better solution

--- go/problem_solving/leet_code/roman_to_int/solution.go
+++ go/problem_solving/leet_code/roman_to_int/solution.go
@@ -1,31 +1,23 @@
 package romantoint
 
 var roman map[string]int = map[string]int{
-	"I":  1,
-	"V":  5,
-	"X":  10,
-	"L":  50,
-	"C":  100,
-	"D":  500,
-	"M":  1000,
-	"IV": 4,
-	"IX": 9,
-	"XL": 40,
-	"XC": 90,
-	"CD": 400,
-	"CM": 900,
+	"I": 1,
+	"V": 5,
+	"X": 10,
+	"L": 50,
+	"C": 100,
+	"D": 500,
+	"M": 1000,
 }
 
-func romanToInt(s string) int {
-	var res int
+func romanToInt(s string) (result int) {
 	for i := 0; i < len(s); i++ {
-		if i+1 < len(s) && roman[string(s[i:i+2])] > 0 {
-			res += roman[string(s[i:i+2])]
-			// go to next letter
-			i++
+		if i+1 < len(s) && roman[string(s[i])] < roman[string(s[i+1])] {
+			// subtract
+			result -= roman[string(s[i])]
 		} else {
-			res += roman[string(s[i])]
+			result += roman[string(s[i])]
 		}
 	}
-	return res
+	return result
 }
