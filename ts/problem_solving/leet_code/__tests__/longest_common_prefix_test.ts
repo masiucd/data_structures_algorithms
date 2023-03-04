cd7af95aa604d5892b9d73f4dfea9b3efb18cab9
masiucd <ciszekmarcell@gmail.com>
2023-03-04 17:49:30 +0100

    feat: Longets common prefix

---
+++ ts/problem_solving/leet_code/__tests__/longest_common_prefix_test.ts
@@ -0,0 +1,10 @@
+import {assertEquals} from "../../../dev_deps.ts"
+import {longestCommonPrefix} from "../longest_common_prefix/solution.ts"
+
+Deno.test("longestCommonPrefix it works with fully list", () => {
+  assertEquals(longestCommonPrefix(["flower", "flow", "flight"]), "fl")
+})
+
+Deno.test("longestCommonPrefix it works with empty string", () => {
+  assertEquals(longestCommonPrefix([""]), "")
+})
