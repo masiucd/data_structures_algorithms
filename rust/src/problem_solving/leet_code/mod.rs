5d5247e96012961498c601a4b59af7c01e29ac81
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2023-03-06 07:55:22 +0100

    feat: plus one rust

--- rust/src/problem_solving/leet_code/mod.rs
+++ rust/src/problem_solving/leet_code/mod.rs
@@ -1,5 +1,6 @@
 pub mod get_concatenation;
+pub mod longest_common_prefix;
+pub mod plus_ons;
+pub mod roman_to_int;
 pub mod single_number;
 pub mod two_sum;
-pub mod roman_to_int;
-pub mod longest_common_prefix;
