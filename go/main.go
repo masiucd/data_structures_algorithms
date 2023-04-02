9786b49a6635264123e0aef88afd95a92b24ae52
masiucd <ciszekmarcell@gmail.com>
2023-04-02 17:26:20 +0200

    clear main

--- go/main.go
+++ go/main.go
@@ -1,11 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println(containsNearbyDuplicate([]int{1, 2, 3, 1}, 3))
-	fmt.Println(containsNearbyDuplicate([]int{1, 0, 1, 1}, 1))
-	fmt.Println(containsNearbyDuplicate([]int{1, 2, 3, 1, 2, 3}, 2))
-}
 
 }
