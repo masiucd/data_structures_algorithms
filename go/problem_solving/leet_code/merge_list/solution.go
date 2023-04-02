e8042562958c8fd15e2d59e79082e6a7382580fb
masiucd <ciszekmarcell@gmail.com>
2023-04-02 17:23:50 +0200

    feat: mergeSortedList GO

--- go/problem_solving/leet_code/merge_list/solution.go
+++ go/problem_solving/leet_code/merge_list/solution.go
@@ -11,7 +11,6 @@ func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
 	var tail = newList
 
 	for list1 != nil && list2 != nil {
-		// build the list backwords!!
 		if list1.Val < list2.Val {
 			tail.Next = list1
 			list1 = list1.Next
