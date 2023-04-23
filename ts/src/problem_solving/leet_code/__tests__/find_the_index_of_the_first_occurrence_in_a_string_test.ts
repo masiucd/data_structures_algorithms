490977505a531a09a48a6dafabfb3f14055ae204
masiucd <ciszekmarcell@gmail.com>
2023-04-23 17:51:01 +0200

    Contains duplicates part 2 TS

--- ts/src/problem_solving/leet_code/__tests__/find_the_index_of_the_first_occurrence_in_a_string_test.ts
+++ ts/src/problem_solving/leet_code/__tests__/find_the_index_of_the_first_occurrence_in_a_string_test.ts
@@ -1,5 +1,5 @@
 import {assertEquals} from "~/dev_deps.ts"
-import {solution} from "../find_the_index_of_the_first_occurrence_in_a_string/solution"
+import {solution} from "../find_the_index_of_the_first_occurrence_in_a_string/solution.ts"
 
 Deno.test("solution it works", () => {
   assertEquals(solution("hello", "ll"), 2)
