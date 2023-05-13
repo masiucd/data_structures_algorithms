25f8b7aa7d5ded8dab1b0f0be36af157e3f9a143
masiucd <ciszekmarcell@gmail.com>
2023-05-13 10:27:53 +0200

    With lib

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,12 +1,11 @@
-use crate::problem_solving::leet_code::count_bits;
-
 mod algo;
 mod data_structures;
 mod problem_solving;
 
+use rust::algo::search::binary_search;
+
 fn main() {
-  let res = count_bits::count_bits(5);
-  // print as binary
-  println!("binary: {:b}", 5);
-  println!("res: {}", res);
+  let xs = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let res = binary_search(&xs, 5);
+  println!("{:?}", res);
 }
