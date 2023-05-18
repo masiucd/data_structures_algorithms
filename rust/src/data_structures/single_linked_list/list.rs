c107631e250fc8c7d68bf0020b897b3747d03bd4
marcell ciszek druzynski <ciszekmarcell@gmail.com>
2023-05-18 16:32:58 +0200

    Build tree

--- rust/src/data_structures/single_linked_list/list.rs
+++ rust/src/data_structures/single_linked_list/list.rs
@@ -1,106 +1,106 @@
-pub struct Node {
-  data: i32,
-  next: Option<Box<Node>>,
-}
+// pub struct Node {
+//   data: i32,
+//   next: Option<Box<Node>>,
+// }
 
-pub trait List {
-  fn new() -> Self;
-  fn append(&mut self, data: i32);
-  fn prepend(&mut self, data: i32);
-  fn remove(&mut self, data: i32);
-  fn get(&self, index: usize) -> Option<&i32>;
-  fn print(&self);
-}
+// pub trait List {
+//   fn new() -> Self;
+//   fn append(&mut self, data: i32);
+//   fn prepend(&mut self, data: i32);
+//   fn remove(&mut self, data: i32);
+//   fn get(&self, index: usize) -> Option<&i32>;
+//   fn print(&self);
+// }
 
-pub struct SingleLinkedList {
-  head: Option<Box<Node>>,
-  tail: Option<Box<Node>>,
-  length: usize,
-}
+// pub struct SingleLinkedList {
+//   head: Option<Box<Node>>,
+//   tail: Option<Box<Node>>,
+//   length: usize,
+// }
 
-impl List for SingleLinkedList {
-  fn new() -> Self {
-    SingleLinkedList {
-      head: None,
-      tail: None,
-      length: 0,
-    }
-  }
+// impl List for SingleLinkedList {
+//   fn new() -> Self {
+//     SingleLinkedList {
+//       head: None,
+//       tail: None,
+//       length: 0,
+//     }
+//   }
 
-  fn append(&mut self, data: i32) {
-    let new_node = Box::new(Node {
-      data,
-      next: None,
-    });
+//   fn append(&mut self, data: i32) {
+//     let new_node = Box::new(Node {
+//       data,
+//       next: None,
+//     });
 
-    match self.tail.take() {
-      Some(old_tail) => {
-        old_tail.next = Some(new_node);
-      }
-      None => {
-        self.head = Some(new_node);
-      }
-    }
+//     match self.tail.take() {
+//       Some(old_tail) => {
+//         old_tail.next = Some(new_node);
+//       }
+//       None => {
+//         self.head = Some(new_node);
+//       }
+//     }
 
-    self.length += 1;
-    self.tail = Some(new_node);
-  }
+//     self.length += 1;
+//     self.tail = Some(new_node);
+//   }
 
-  fn prepend(&mut self, data: i32) {
-    let new_node = Box::new(Node {
-      data,
-      next: self.head.take(),
-    });
+//   fn prepend(&mut self, data: i32) {
+//     let new_node = Box::new(Node {
+//       data,
+//       next: self.head.take(),
+//     });
 
-    if self.head.is_none() {
-      self.tail = Some(new_node.clone());
-    }
+//     if self.head.is_none() {
+//       self.tail = Some(new_node.clone());
+//     }
 
-    self.head = Some(new_node);
-    self.length += 1;
-  }
+//     self.head = Some(new_node);
+//     self.length += 1;
+//   }
 
-  fn remove(&mut self, data: i32) {
-    let mut current_node = self.head.take();
+//   fn remove(&mut self, data: i32) {
+//     let mut current_node = self.head.take();
 
-    if let Some(mut node) = current_node {
-      if node.data == data {
-        self.head = node.next.take();
-        self.length -= 1;
-        return;
-      }
+//     if let Some(mut node) = current_node {
+//       if node.data == data {
+//         self.head = node.next.take();
+//         self.length -= 1;
+//         return;
+//       }
 
-      while let Some(next_node) = node.next.take() {
-        if next_node.data == data {
-          node.next = next_node.next;
-          self.length -= 1;
-          break;
-        }
-        node = next_node;
-      }
-    }
-  }
+//       while let Some(next_node) = node.next.take() {
+//         if next_node.data == data {
+//           node.next = next_node.next;
+//           self.length -= 1;
+//           break;
+//         }
+//         node = next_node;
+//       }
+//     }
+//   }
 
-  fn get(&self, index: usize) -> Option<&i32> {
-    if index >= self.length {
-      return None;
-    }
+//   fn get(&self, index: usize) -> Option<&i32> {
+//     if index >= self.length {
+//       return None;
+//     }
 
-    let mut current_node = self.head.as_ref();
+//     let mut current_node = self.head.as_ref();
 
-    for _ in 0..index {
-      current_node = current_node.unwrap().next.as_ref();
-    }
+//     for _ in 0..index {
+//       current_node = current_node.unwrap().next.as_ref();
+//     }
 
-    current_node.map(|node| &node.data)
-  }
+//     current_node.map(|node| &node.data)
+//   }
 
-  fn print(&self) {
-    let mut current_node = self.head.as_ref();
+//   fn print(&self) {
+//     let mut current_node = self.head.as_ref();
 
-    while let Some(node) = current_node {
-      println!("{}", node.data);
-      current_node = node.next.as_ref();
-    }
-  }
-}
+//     while let Some(node) = current_node {
+//       println!("{}", node.data);
+//       current_node = node.next.as_ref();
+//     }
+//   }
+// }
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,11 +1,44 @@
 // Name of the project: rust
-use rust::algos::bits::count;
-use rust::common;
-use rust::math;
+// use rust::algos::bits::count;
+// use rust::common;
+// use rust::math;
+
+#[derive(Debug)]
+struct Item {
+  id: u8,
+  title: String,
+  parent_id: Option<u8>,
+}
+
+#[derive(Debug, Clone)]
+struct TreeNode {
+  id: u8,
+  title: String,
+  parent_id: Option<u8>,
+  children: Vec<TreeNode>,
+}
 
 fn main() {
-  let xs = math::prime::prime_nums_from_range(1, 10);
-  println!("{:?}", xs);
-  let r = math::prime::is_prime(4);
-  println!("{}", r);
+  let xs: Vec<Item> = vec![
+    Item { id: 1, title: "Europe".to_string(), parent_id: None },
+    Item { id: 2, title: "Asia".to_string(), parent_id: None },
+    Item { id: 3, title: "France".to_string(), parent_id: Some(1) },
+    Item { id: 4, title: "Thailand".to_string(), parent_id: Some(2) },
+    Item { id: 5, title: "Paris".to_string(), parent_id: Some(3) }
+  ];
+
+  let r = build_tree(&xs, None);
+  println!("{:#?}", r);
+}
+
+fn build_tree(xs: &Vec<Item>, parent_id: Option<u8>) -> Vec<TreeNode> {
+  xs.iter()
+    .filter(|x| x.parent_id == parent_id)
+    .map(|x| TreeNode {
+      title: x.title.clone(),
+      id: x.id,
+      parent_id: x.parent_id,
+      children: build_tree(xs, Some(x.id)),
+    })
+    .collect()
 }
