37b0ffe6bd037ec23cead15793d30631effa4f4f
masiucd <ciszekmarcell@gmail.com>
2023-09-30 12:36:16 +0200

    Min stack with GO

--- algorithms-data-structures/go/src/data-structures/stacks/min-stack/min-stack.go
+++ algorithms-data-structures/go/src/data-structures/stacks/min-stack/min-stack.go
@@ -1,22 +1,4 @@
-package main
-
-import "fmt"
-
-func main() {
-	minStack := Constructor()
-	minStack.Push(-2)
-	minStack.Push(0)
-	minStack.Push(-3)
-	printStacks(minStack)
-	min := minStack.GetMin() // return -3
-	fmt.Println("MIN", min)
-	minStack.Pop()
-	top := minStack.Top() // return 0
-	fmt.Println("top", top)
-	min = minStack.GetMin() // return -2
-	fmt.Println("MIN", min)
-
-}
+package minstack
 
 type MinStack struct {
 	stack []int
@@ -58,19 +40,8 @@ func (ms *MinStack) Top() int {
 }
 
 func (ms *MinStack) GetMin() int {
+	if len(ms.mins) == 0 {
+		return -1
+	}
 	return ms.mins[len(ms.mins)-1]
-}
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * obj := Constructor();
- * obj.Push(val);
- * obj.Pop();
- * param_3 := obj.Top();
- * param_4 := obj.GetMin();
- */
-
-func printStacks(m MinStack) {
-	fmt.Println("STACK ", m.stack)
-	fmt.Println("MINS ", m.mins)
 }
---
+++ algorithms-data-structures/rs/src/data_structures/stacks/min_stack/min-stack.md
@@ -0,0 +1,22 @@
+# Min stack
+
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+## Example:
+
+```bash
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin();   --> Returns -3.
+minStack.pop();
+minStack.top();      --> Returns 0.
+minStack.getMin();   --> Returns -2.
+```
+
+You must implement a solution with O(1) time complexity for each function.
+
+
+
+
