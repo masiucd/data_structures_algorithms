3fc1ca2c1ce859f46da586c02b8cc79b06c49973
masiucd <ciszekmarcell@gmail.com>
2023-10-01 10:33:38 +0200

    Stack ts

---
+++ algorithms-data-structures/ts/data-structures/stacks/stack/stack.md
@@ -0,0 +1,5 @@
+# Stack
+
+## Description
+
+Implement a stack data structure where the push and pop methods are both O(1) operations.
---
+++ algorithms-data-structures/ts/data-structures/stacks/stack/stack.ts
@@ -0,0 +1,34 @@
+export class Stack {
+  private items: any[];
+  constructor() {
+    this.items = [];
+  }
+
+  push(element: any) {
+    this.items.push(element);
+  }
+
+  pop() {
+    return this.items.pop();
+  }
+
+  peek() {
+    return this.items[this.items.length - 1];
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  size() {
+    return this.items.length;
+  }
+
+  clear() {
+    this.items = [];
+  }
+
+  print() {
+    console.log(this.items.toString());
+  }
+}
