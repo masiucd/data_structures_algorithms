563b01fe6bbfb2bf99b7d7361be902f2b6e3d04d
marcell ciszek druzynski <ciszekmarcell@gmail.com>
2023-10-12 10:35:59 +0200

    Linked list GO

---
+++ algorithms-data-structures/go/src/algorithms/data-structures/README.md
@@ -0,0 +1,13 @@
+# Data structures in Go
+
+## Table of Contents
+
+- [Array](#array)
+- [Linked List](#linked-list)
+- [Stack](#stack)
+- [Queue](#queue)
+- [Set](#set)
+- [Map](#map)
+- [Tree](#tree)
+- [Graph](#graph)
+- [Heap](#heap)
---
+++ algorithms-data-structures/go/src/algorithms/data-structures/linked-list/single.go
@@ -0,0 +1,32 @@
+package linkedlist
+
+type SingleList struct {
+	Head *SingleNode
+	Tail *SingleNode
+	Size int
+}
+
+func (l *SingleList) Append(value int) {
+
+}
+func (l *SingleList) Prepend(value int) {
+
+}
+func (l *SingleList) InsertAt(value, index int) {
+
+}
+func (l *SingleList) Search(value int) bool {
+	return false
+}
+
+func (l *SingleList) Get(index int) int {
+	return 0
+}
+
+func (l *SingleList) Delete(value int) {
+
+}
+
+func (l *SingleList) Traverse() {}
+
+func Reverse() {}
