964ee9af9d2c53ca3d0bb5349f33ce8b67683af2
masiucd <ciszekmarcell@gmail.com>
2023-10-14 12:36:49 +0200

    idea

---
+++ web/.contentlayer/.cache/v0.3.4/compiled-contentlayer-config-ZAOHD34S.mjs
@@ -0,0 +1,28 @@
+// contentlayer.config.ts
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+var Post = defineDocumentType(() => ({
+  name: "Post",
+  filePathPattern: `**/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    title: { type: "string", required: true },
+    date: { type: "date", required: true },
+    tags: { type: "list", required: true }
+  },
+  computedFields: {
+    url: {
+      type: "string",
+      resolve: (post) => `/posts/${post._raw.flattenedPath}`
+    },
+    slug: {
+      type: "string",
+      resolve: (post) => post._raw.flattenedPath
+    }
+  }
+}));
+var contentlayer_config_default = makeSource({ contentDirPath: "posts", documentTypes: [Post] });
+export {
+  Post,
+  contentlayer_config_default as default
+};
+//# sourceMappingURL=compiled-contentlayer-config-ZAOHD34S.mjs.map
