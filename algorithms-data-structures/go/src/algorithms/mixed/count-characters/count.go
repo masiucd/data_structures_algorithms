23a51cc29ce556d6c8b3c552850c648167fb3a5c
masiucd <ciszekmarcell@gmail.com>
2023-12-02 17:20:35 +0100

    Solution using array

--- algorithms-data-structures/go/src/algorithms/mixed/count-characters/count.go
+++ algorithms-data-structures/go/src/algorithms/mixed/count-characters/count.go
@@ -31,3 +31,32 @@ func wordToCharMap(word string) map[string]int {
 	}
 	return charMap
 }
+
+func countCharactersTwo(words []string, chars string) int {
+	var result int
+	var count [26]int
+	for _, ch := range chars {
+		count[ch-'a']++
+	}
+	for _, word := range words {
+		// make a copy of count
+		currentWord := count
+		// check if word is ok
+		isOk := true
+		for _, ch := range word {
+			// if currentWord[ch-'a'] > 0, then decrement it
+			if currentWord[ch-'a'] > 0 {
+				currentWord[ch-'a']--
+			} else {
+				// if currentWord[ch-'a'] == 0, then break
+				isOk = false
+				break
+			}
+		}
+		if isOk {
+			// if word is ok, then add its length to result
+			result += len(word)
+		}
+	}
+	return result
+}
--- algorithms-data-structures/go/src/algorithms/mixed/count-characters/count_test.go
+++ algorithms-data-structures/go/src/algorithms/mixed/count-characters/count_test.go
@@ -17,3 +17,15 @@ func TestCountCharacters(t *testing.T) {
 	res = countCharacters(words, chars)
 	assert.Equal(t, 10, res)
 }
+
+func TestCountCharactersTwo(t *testing.T) {
+	words := []string{"cat", "bt", "hat", "tree"}
+	chars := "atach"
+	res := countCharactersTwo(words, chars)
+	assert.Equal(t, 6, res)
+
+	words = []string{"hello", "world", "leetcode"}
+	chars = "welldonehoneyr"
+	res = countCharactersTwo(words, chars)
+	assert.Equal(t, 10, res)
+}
