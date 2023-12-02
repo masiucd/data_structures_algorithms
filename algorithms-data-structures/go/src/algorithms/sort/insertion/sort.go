4cce05772df6291ec091db5a3812769336c2c59a
masiucd <ciszekmarcell@gmail.com>
2023-12-02 16:34:40 +0100

    BST

--- algorithms-data-structures/go/src/algorithms/sort/insertion/sort.go
+++ algorithms-data-structures/go/src/algorithms/sort/insertion/sort.go
@@ -4,6 +4,7 @@ func insertionSort(arr []int) []int {
 	for i := 1; i < len(arr); i++ {
 		j := i
 		// as long j is in bounce and arr[j] < arr[j-1], then we swap
+		// current is less than previous
 		for j > 0 && arr[j] < arr[j-1] {
 			//swapping
 			arr[j], arr[j-1] = arr[j-1], arr[j]
