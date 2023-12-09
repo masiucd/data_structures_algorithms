ddc214deda8b6bc80de473779908b8d88db3e67b
masiucd <ciszekmarcell@gmail.com>
2023-12-09 11:59:20 +0100

    BST WIP

--- algorithms-data-structures/go/go.mod
+++ algorithms-data-structures/go/go.mod
@@ -2,11 +2,12 @@ module go-ds
 
 go 1.21.1
 
+require github.com/stretchr/testify v1.8.4
+
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gohugoio/hugo v0.121.1
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/repeale/fp-go v0.11.1 // indirect
-	github.com/stretchr/objx v0.5.0 // indirect
-	github.com/stretchr/testify v1.8.4 // indirect
+	github.com/repeale/fp-go v0.11.1
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- algorithms-data-structures/go/main.go
+++ algorithms-data-structures/go/main.go
@@ -2,34 +2,87 @@ package main
 
 import (
 	"fmt"
-	"regexp"
-	"strings"
 )
 
+type Node struct {
+	value int
+	left  *Node
+	right *Node
+}
+
+type Tree interface {
+	Insert(value int)
+	Search(value int) bool
+	Remove(value int)
+	Traverse()
+	Find(value int) *Node
+	Contains(value int) bool
+	BFS() []int
+	DFS(order string) []int // in, pre, post
+
+}
+
+func NewBst() Tree {
+	return &Bst{}
+}
+
+type Bst struct {
+	root *Node
+}
+
+func (t *Bst) Insert(value int) {
+	if t.root == nil {
+		t.root = &Node{value: value}
+		return
+	}
+	insert(t.root, value)
+}
+
+func insert(node *Node, value int) *Node {
+	if node == nil {
+		return &Node{value: value}
+	}
+	if value < node.value {
+		node.left = insert(node.left, value)
+	} else if value > node.value {
+		node.right = insert(node.right, value)
+	}
+	return node
+}
+
+func (t *Bst) Search(value int) bool   { return false }
+func (t *Bst) Remove(value int)        {}
+func (t *Bst) Traverse()               {}
+func (t *Bst) Find(value int) *Node    { return nil }
+func (t *Bst) Contains(value int) bool { return false }
+
+func (t *Bst) BFS() []int {
+	var result []int
+	q := []Node{*t.root}
+	for len(q) > 0 {
+		node := q[0]
+		q = q[1:]
+		result = append(result, node.value)
+		if node.left != nil {
+			q = append(q, *node.left)
+		}
+		if node.right != nil {
+			q = append(q, *node.right)
+		}
+	}
+	return result
+}
+
+func (t *Bst) DFS(order string) []int { return nil }
+
 func main() {
-	res := isPalindrome("A man a plan a canal Panama")
-	fmt.Println(res)
-	res = isPalindrome("race a car")
-	fmt.Println(res)
-	res = isPalindrome("booo")
-	fmt.Println(res)
-}
+	bst := NewBst()
+	bst.Insert(10)
+	bst.Insert(5)
+	bst.Insert(15)
+	bst.Insert(2)
+	bst.Insert(1)
 
-func isPalindrome(s string) bool {
-	if len(s) < 2 {
-		return true
-	}
-	s = strings.ToLower(s)
-	regexp, _ := regexp.Compile("[^a-z0-9]+")
-	s = regexp.ReplaceAllString(s, "")
+	fmt.Println(bst.BFS())
 
-	return reverseString(s) == s
-}
-
-func reverseString(s string) string {
-	var reversed string
-	for i := len(s) - 1; i >= 0; i-- {
-		reversed += string(s[i])
-	}
-	return reversed
 }
