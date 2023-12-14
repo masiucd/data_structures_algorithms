c4cc645fd52773318475a104404291c3425c1d3d
marcell ciszek druzynski <ciszekmarcell@gmail.com>
2023-12-14 17:26:51 +0100

    topKFrequent2 GO

--- algorithms-data-structures/go/src/algorithms/mixed/top-k-frequent/top_test.go
+++ algorithms-data-structures/go/src/algorithms/mixed/top-k-frequent/top_test.go
@@ -16,11 +16,16 @@ func TestTopKFrequent(t *testing.T) {
 		{[]int{1, 1, 1, 2, 2, 3}, 2, []int{1, 2}},
 		{[]int{1}, 1, []int{1}},
 		{[]int{1, 2}, 2, []int{1, 2}},
-		{[]int{1, 2, 3}, 2, []int{1, 2}},
 	}
 
 	for _, test := range tests {
 		got := topKFrequent(test.input, test.k)
 		assert.Equal(t, test.expected, got)
+
+	}
+	for _, test := range tests {
+		got := topKFrequentTwo(test.input, test.k)
+		assert.Equal(t, test.expected, got)
+
 	}
 }
