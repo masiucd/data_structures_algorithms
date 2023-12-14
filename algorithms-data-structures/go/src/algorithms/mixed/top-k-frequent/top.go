c6ec58eeae3da45857eaa505230c5965bd621ded
marcell ciszek druzynski <ciszekmarcell@gmail.com>
2023-12-14 17:25:28 +0100

    topKFrequent2 GO

--- algorithms-data-structures/go/src/algorithms/mixed/top-k-frequent/top.go
+++ algorithms-data-structures/go/src/algorithms/mixed/top-k-frequent/top.go
@@ -20,3 +20,28 @@ func topKFrequent(nums []int, k int) []int {
 	}
 	return result
 }
+
+func topKFrequentTwo(numbers []int, k int) []int {
+	var store = make(map[int]int)
+	for _, v := range numbers {
+		store[v]++
+	}
+	// bucket that stores the numbers with the same frequency
+	bucket := make([][]int, len(numbers)+1)
+	for k, v := range store {
+		bucket[v] = append(bucket[v], k)
+	}
+
+	var res []int
+	// iterate the bucket from the end
+	for i := len(bucket) - 1; i >= 0; i-- {
+		// if the bucket is not empty and k is not 0
+		if len(bucket[i]) > 0 && k > 0 {
+			// append the numbers to the result
+			res = append(res, bucket[i]...)
+			// decrement k by the length of the bucket
+			k -= len(bucket[i])
+		}
+	}
+	return res
+}
