94b319a6b2579379d913d78f5635b552c65e4b3a
masiucd <ciszekmarcell@gmail.com>
2023-12-24 10:41:23 +0100

    Init

--- algorithms-data-structures/go/main.go
+++ algorithms-data-structures/go/main.go
@@ -1,42 +1,5 @@
 package main
 
-<<<<<<< HEAD
-import (
-	"fmt"
-)
-
-func main() {
-
-	xs := []int{1, 1, 1, 1, 2, 2, 2, 3}
-	k := 2
-	res := topKFrequent(xs, k)
-	fmt.Println(res)
-}
-
-func topKFrequent(numbers []int, k int) []int {
-	var store = make(map[int]int)
-	for _, v := range numbers {
-		store[v]++
-	}
-	// bucket that stores the numbers with the same frequency
-	bucket := make([][]int, len(numbers)+1)
-	for k, v := range store {
-		bucket[v] = append(bucket[v], k)
-	}
-
-	var res []int
-	// iterate the bucket from the end
-	for i := len(bucket) - 1; i >= 0; i-- {
-		// if the bucket is not empty and k is not 0
-		if len(bucket[i]) > 0 && k > 0 {
-			// append the numbers to the result
-			res = append(res, bucket[i]...)
-			// decrement k by the length of the bucket
-			k -= len(bucket[i])
-		}
-	}
-	return res
-=======
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
@@ -74,5 +37,4 @@ func hasPathSum(root *TreeNode, targetSum int) bool {
 		return true
 	}
 	return false
->>>>>>> ea5f6fd5bc901ff554c38d77e30a75d007512a6f
 }
