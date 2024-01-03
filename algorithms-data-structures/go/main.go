0a8b17fd723ec0167aac694bdfc2cbb2945aeb08
marcell ciszek druzynski <ciszekmarcell@gmail.com>
2024-01-03 11:52:08 +0100

    Refactor topKFrequent function in main.go

--- algorithms-data-structures/go/main.go
+++ algorithms-data-structures/go/main.go
@@ -36,43 +36,4 @@ func topKFrequent(numbers []int, k int) []int {
 		}
 	}
 	return res
-=======
-type TreeNode struct {
-	Val   int
-	Left  *TreeNode
-	Right *TreeNode
-}
-
-func main() {
-	root := &TreeNode{Val: 1}
-	root.Left = &TreeNode{Val: 2}
-	root.Right = &TreeNode{Val: 3}
-	targetSum := 4
-	res := hasPathSum(root, targetSum)
-	println(res)
-}
-
-/**
- * Definition for a binary tree node.
- * type TreeNode struct {
- *     Val int
- *     Left *TreeNode
- *     Right *TreeNode
- * }
- */
-func hasPathSum(root *TreeNode, targetSum int) bool {
-	if root == nil {
-		return false
-	}
-	if root.Val == targetSum && root.Left == nil && root.Right == nil {
-		return true
-	}
-	if hasPathSum(root.Left, targetSum-root.Val) {
-		return true
-	}
-	if hasPathSum(root.Right, targetSum-root.Val) {
-		return true
-	}
-	return false
->>>>>>> ea5f6fd5bc901ff554c38d77e30a75d007512a6f
 }
