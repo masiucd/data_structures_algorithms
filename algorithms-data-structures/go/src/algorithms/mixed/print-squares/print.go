b1f9b1cd0c0efbb370cde7308ee582d3033d3a80
marcell ciszek druzynski <ciszekmarcell@gmail.com>
2024-01-10 11:24:44 +0100

    Refactor printSquaresTopDown and printSquaresDownTop functions to return a string instead of printing

--- algorithms-data-structures/go/src/algorithms/mixed/print-squares/print.go
+++ algorithms-data-structures/go/src/algorithms/mixed/print-squares/print.go
@@ -1,23 +1,25 @@
 package printsquares
 
-import "fmt"
-
-func printSquaresTopDown(n int) {
+func printSquaresTopDown(n int) string {
+	var result string
 	for i := 0; i < n; i++ {
 		x := ""
 		for j := i; j < n; j++ {
 			x += "#"
 		}
-		fmt.Println(x)
+		result += x + "\n"
 	}
+	return result
 }
 
-func printSquaresDownTop(n int) {
+func printSquaresDownTop(n int) string {
+	var result string
 	for i := 0; i < n; i++ {
 		x := ""
 		for j := 0; j < i; j++ {
 			x += "#"
 		}
-		fmt.Println(x)
+		result += x + "\n"
 	}
+	return result
 }
