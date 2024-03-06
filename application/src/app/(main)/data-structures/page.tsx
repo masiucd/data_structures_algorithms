a8b80cf5de7178872befdfb3a22ad2203af20e70
masiucd <ciszekmarcell@gmail.com>
2024-03-06 15:57:42 +0100

    Remove mocked data

--- application/src/app/(main)/data-structures/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function DataStructuresPage() {
-	return <div>DataStrucutresPage</div>;
-}
--- application/src/components/mdx.tsx
+++
@@ -1,6 +0,0 @@
-import {MDXRemote} from "next-mdx-remote/rsc";
-import {highlight} from "sugar-high";
-
-export function Mdx() {
-	return <div>Mdx</div>;
-}
