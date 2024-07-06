415f19d78afbebf8ca909aa913f8c461b907d616
masiucd <ciszekmarcell@gmail.com>
2024-07-06 19:12:46 +0200

    feat: Add PersistenceV2 function to calculate the persistence of a number
    The code changes include the addition of a new function `PersistenceV2` in the `persistence.go` file. This function calculates the persistence of a number by multiplying its digits until it becomes a single digit. It also includes tests in the `persistence_test.go` file to ensure the correctness of the implementation.
    
    This commit adds the `PersistenceV2` function as an improvement to the existing codebase, providing an alternative approach to calculate persistence.

--- go/src/algorithms/numbers/persistance/persistence.go
+++ go/src/algorithms/numbers/persistance/persistence.go
@@ -24,6 +24,30 @@ func Persistence(n int) int {
 	return persistenceCount
 }
 
+func PersistenceV2(n int) int {
+	persistenceCount := 0
+	if isSingleDigit(n) {
+		return persistenceCount
+	}
+
+	for !isSingleDigit(n) {
+		product := 1
+		for n > 0 {
+			digit := n % 10
+			// Early termination if product becomes zero
+			if digit == 0 {
+				persistenceCount++
+				return persistenceCount
+			}
+			product *= digit
+			n /= 10
+		}
+		n = product
+		persistenceCount++
+	}
+	return persistenceCount
+}
+
 func isSingleDigit(n int) bool {
 	return n >= 0 && n <= 9
 }
